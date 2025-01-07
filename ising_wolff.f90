01f051f06fc5c51204a7e4d8ca70e2965bff1473
carlospatinopd <c.patino02@ufromail.cl>
2025-01-07 20:21:45 -0300

    .

--- ising_wolff.f90
+++ ising_wolff.f90
@@ -12,10 +12,10 @@ program ising_wolff
     !------------------------------------------------------------------------!
     !                               variables                                !
     !------------------------------------------------------------------------!
-    integer :: it, i, j, ip, im, jp, jm, Si, n_add
+    integer :: it, i, j, Si, n_add, ic
     real(8) :: r
-    integer, dimension(nx,nx) :: S
-    integer, dimension(2,4) :: cluster_add
+    integer, dimension(nx,nx) :: S, cluster
+    integer, dimension(2,4) :: s_add
 
     !------------------------------------------------------------------------!
     !                        Monte Carlo Wolff Method                        !
@@ -24,10 +24,22 @@ program ising_wolff
     call initial_state(S) ! asign random values to the spins (1 or -1)
 
     do it = 1,N
+        cluster = 1
         call random_spin(i,j) ! randomly choose a spin
+        cluster(i,j) = -1
         Si = S(i,j) ! value of the choosen spin
-        call pbc(ip,im,jp,jm) ! neighbors with periodic boundary conditions
-
+        call cluster_formation(i,j,n_add,s_add)
+        do while (n_add > 0)
+            do ic = 1,n_add
+                call cluster_formation(s_add(1,ic),s_add(1,ic),n_add,s_add)
+                cluster(s_add(1,ic),s_add(2,ic)) = -1
+            end do
+        end do
+        do i = 1,nx
+            do j = 1,ny
+                S(i,j) = S(i,j)*cluster(i,j)
+            end do 
+        end do 
     end do
 
     !------------------------------------------------------------------------!
@@ -57,39 +69,44 @@ program ising_wolff
         j = 1+int(r*ny)
     end subroutine random_spin
 
-    ! neighbors and periodic boudary conditions
-    subroutine pbc(ip,im,jp,jm)
-        integer, intent(out) :: ip, im, jp, jm
+    ! neighbors with periodic boudary conditions for cluster formation
+    subroutine cluster_formation(i,j,n_add,s_add)
+        integer, intent(in) :: i, j
+        integer :: ip, im, jp, jm
+        integer, intent(out) :: s_add(2,4), n_add
 
         if (i == nx) then; ip = 1; else; ip = i+1; end if
         if (i == 1) then; im = nx; else; im = i-1; end if
         if (j == ny) then; jp = 1; else; jp = j+1; end if
         if (j == 1) then; jm = ny; else;  jm = j-1; end if
-        
-        cluster_add(1,n_add) = i
-        cluster_add(2,n_add) = j
 
         n_add = 0
-        if (S(ip,j) == Si) then
+        call random_number(r)
+        if (S(ip,j) == Si .AND. r >= p) then
             n_add = n_add+1
-            cluster_add(1,n_add) = ip
-            cluster_add(2,n_add) = j
+            s_add(1,n_add) = ip
+            s_add(2,n_add) = j
         end if
-        if (S(im,j) == Si) then
+        call random_number(r)
+        if (S(im,j) == Si .AND. r >= p) then
             n_add = n_add+1
-            cluster_add(1,n_add) = im
-            cluster_add(2,n_add) = j
+            s_add(1,n_add) = im
+            s_add(2,n_add) = j
         end if
-        if (S(i,jp) == Si) then
+        call random_number(r)
+        if (S(i,jp) == Si .AND. r >= p) then
             n_add = n_add+1
-            cluster_add(1,n_add) = i
-            cluster_add(2,n_add) = jp
+            s_add(1,n_add) = i
+            s_add(2,n_add) = jp
         end if
-        if (S(i,jm) == Si) then
+        call random_number(r)
+        if (S(i,jm) == Si .AND. r >= p) then
             n_add = n_add+1
-            cluster_add(1,n_add) = i
-            cluster_add(2,n_add) = jm
+            s_add(1,n_add) = i
+            s_add(2,n_add) = jm
         end if
-    end subroutine pbc
+
+        Si = S(i,j)
+    end subroutine cluster_formation
 
 end program ising_wolff
